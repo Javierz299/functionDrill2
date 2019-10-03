2fbbf2b205c002a69d1bf15ea9730f9517609782
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-03 18:20:24 -0400

    add a space to decoder

--- index.js
+++ index.js
@@ -24,6 +24,36 @@ function beyond(num){
 beyond();
 
 function decode(encode){
-  
+  const decoder = {
+    a:2,
+    b:3,
+    c:4,
+    d:5,
+    space: '',
+  };
+  const cracked = [];
 
+  let words = encode.split(' ');
+  //console.log(words);
+
+
+  for(let i = 0; i < words.length; i++){
+    for(let prop in decoder){
+    //console.log(prop);
+    //console.log(words[i].slice(0,1));
+      if(words[i].slice(0,1) === prop){
+        //console.log('found a match');
+        cracked.push(words[i][decoder[prop] - 1]);
+        //figure out how to add space with out multiple spaces due to loop
+        //still working on this
+      } if(words.slice(0,1) !== prop){
+        //cracked.push(words[decoder['space']]);
+      }
+    }
+  }
+
+  console.log(cracked.join(''));
+  return cracked.join('');
 }
+let codedMessage = 'craft block argon meter bells brown croon droop';
+decode(codedMessage);
